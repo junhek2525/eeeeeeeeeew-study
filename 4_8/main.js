305fe9ff5e4da426c60f2da469737b3d1543f9d4
junhek2525 <junhek2525@gmail.com>
2024-04-08 21:20:01 +0900

    4_8

---
+++ 4_8/main.js
@@ -0,0 +1,18 @@
+const mtName = "기린"
+const email = "기린@gmail.com"
+const hello = `Hello ${myName}??`
+// boolean
+console.log(hello)
+
+const number = 20;
+const number2 = 10.55;
+
+const checked = True;
+const isShow = False;
+// undefined
+console.log(checked);
+
+let undef;
+const obj = {abc:123};
+
+
