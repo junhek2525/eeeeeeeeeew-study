e3da505a617ce0e423e38afc8d4ebbe11c6ab55f
junhek2525 <junhek2525@gmail.com>
2024-04-29 21:11:38 +0900

    4_29

---
+++ 4_29/main.js
@@ -0,0 +1,56 @@
+// if else prompt
+
+//  let ee = prompt("시간을 입력하세요.");
+//  if(ee>12){
+//     console.log("오후");
+//  }else{
+//     console.log("오후전");
+//  }
+
+
+//  if(ee>12){
+//     alert("오후");
+//  }else{
+//     alert("오전");
+//  }
+
+// let 달러 = prompt("달러내놔");
+
+// alert(달러 ,"$");
+// let 돈 = 달러 * 1300
+
+
+// alert(돈,"원");
+
+console.log(2+2-2*2/2*2)
+let A = "aaa";
+console.log(typeof(A));
+
+let a = "가나다"+"라마바사";
+console.log(a);
+let num = 123+456;
+console.log(num);
+
+
+
+let score = Number(prompt("학점을 입력하셍요",'학점'))
+
+if(score>=4.5){
+    alert("신");
+}else if(score>=4.0){
+    alert("교수님");
+}else if(score>=3.0){
+    alert("인간");
+}else if(score>=2.0){
+    alert("돌고래");
+}else if(score>=1.0){
+    alert("플랑크톤");
+}else{
+    alert("4.5보다 크거나 1.0보다 작음")
+}
+
+    
+
+for(let i =0; i<10; i++){
+    console.log(`${i}번 반복`);
+}
