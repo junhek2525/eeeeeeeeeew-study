e652efff15e80ad7beac8d92e36f06019c75edc1
junhek2525 <junhek2525@gmail.com>
2024-06-10 21:09:20 +0900

    6_10

---
+++ 6_10/firstapp/src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import MyComponent from './MyComponent';
+
+import Counter from './counter';
+
+const App = () => {
+  return <Counter />;
+};
+// function App() {
+//   // return (
+//     // <div className="App">
+
+//     const name = '리액트';
+//     return <div className="react">{name}</div>;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+//     //   {/* <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header> */}
+//     // </div>
+//   // );
+// }
+
+// extends Component
+
+// function App(){
+
+//   return <MyComponent name={3}>리액트</MyComponent>;
+//   // render() {
+//   //   const name = 'react';
+//   //   return <div className="react">{name}</div>;
+//   // }
+// }
+
+export default App;
