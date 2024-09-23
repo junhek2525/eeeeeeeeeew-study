5703f9384a99cbc023ef82142d97aaa745b02a47
junhek2525 <junhek2525@gmail.com>
2024-09-23 20:14:57 +0900

    9_23

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom"
 import styled from "styled-components"
 import { useEffect, useState } from "react";
 import { Nav } from "react-bootstrap";
+import '../App.css';
 
 
 
@@ -16,6 +17,34 @@ import { Nav } from "react-bootstrap";
 //     color : black;
 //     padding : 10px;
 // `
+
+function TabContent(props)
+{
+    let [fade, setFade] = useState('')
+    useEffect(() =>{
+        setTimeout(()=>{ setFade('end')},100) 
+        return () => {
+            setFade('')
+        }
+
+    },[props.탭])
+
+    return (
+        <div className={'start '+ fade}>
+            {[<div>내용 0</div>,<div>내용 1</div>,<div>내용 2</div>][props.탭]}
+        </div>
+    )
+    // if(props.탭 === 0){
+    //   return  <div>내용 0</div>
+    // }
+    // if(props.탭 === 1){
+    //     return  <div>내용 1</div>
+    // }
+    // if(props.탭 === 2){
+    //     return  <div>내용 2</div>
+    // }
+}
+
 function Detail(props){
 // useEffect(() => {
 //     console.log("안녕안녕");
@@ -23,13 +52,21 @@ function Detail(props){
 //     //     document.getElementById("yellow").style.display = "None";
 //     // }, 2000);
 // });
-     
-    let [count, setCount] = useState(0);
+
     let {id} = useParams();
     let myItem = props.shoes.find(function(x){
         return x.id == id
     });
+    let [탭, 탭변경] = useState(0)
 
+    let [fade2, setFade2] = useState('')
+    useEffect(() =>{
+        setTimeout(()=>{ setFade2('end')},100) 
+        return () => {
+            setFade2('')
+        }
+
+    },[])
     // let [alert, setAlert] = useState(true)
     // useEffect(() => {
     //    let timer =  setTimeout(() => {setAlert(false)}, 2000);
@@ -37,9 +74,10 @@ function Detail(props){
     //     clearTimeout(timer);
     //    }
     // })
-// 3WEM
+
+    
     return(
-        <div className="container">   
+        <div className={'container start ' + fade2}>   
             <div className="row">
                 <div className="col-md-6">
                 <img src="/shoe1.png" width="100%"></img>
@@ -59,27 +97,17 @@ function Detail(props){
                     {/* </Box> */} 
                     <Nav variant="tabs" defaultActiveKey="link1">
                     <Nav.Item>
-                            <Nav.Link eventKey="link0">버튼 0</Nav.Link>
+                            <Nav.Link onClick={()=>{탭변경(0)}}>버튼 0</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link eventKey="link1">버튼 1</Nav.Link>
+                            <Nav.Link onClick={()=>{탭변경(1)}}>버튼 1</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link eventKey="link2">버튼 2</Nav.Link>
+                            <Nav.Link onClick={()=>{탭변경(2)}}>버튼 2</Nav.Link>
                         </Nav.Item>
-
-                        <Nav.Item>
-                            <Nav.Link eventKey="link3">버튼 3</Nav.Link>
-                        </Nav.Item>
-                        <div>내용 0</div>
-                        <div>내용 1</div>
-                        <div>내용 2</div>
-                        <div>내용 3</div>
-
-
-
 
                     </Nav>
+                        <TabContent 탭={탭}/>
                 </div>
             </div>
         </div>
